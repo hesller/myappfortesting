087b9225fb3ceda0a6410cda07a8ec75e9a8c056
Hesller <eng.hesller@gmail.com>
2018-11-17 23:05:15 -0300

    the final test in 17-11-2018

--- App.js
+++ App.js
@@ -20,8 +20,8 @@ const instructions = Platform.select({
 });
 
 const codePushOptions = {
-  installMode: codePush.InstallMode.ON_NEXT_RESTART,
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME
+  installMode: CodePush.InstallMode.ON_NEXT_RESTART,
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME
 };
 
 
