518cdae9fbf44bdbb5bd8718c624bddaff4763db
Hesller <eng.hesller@gmail.com>
2018-11-18 11:04:21 -0300

    Fith change on Sunday 11-18-2018

--- android/app/src/main/java/com/myappfortesting/MainActivity.java
+++ android/app/src/main/java/com/myappfortesting/MainActivity.java
@@ -8,23 +8,7 @@ import com.facebook.react.ReactRootView;
 import com.microsoft.codepush.react.CodePush;
 
 public class MainActivity extends ReactActivity {
-
-    private ReactRootView mReactRootView;
-    private ReactInstanceManager mReactInstanceManager;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        mReactInstanceManager = ReactInstanceManager.builder()
-                // ...
-                // Add CodePush package
-                .addPackage(new CodePush("a823PWaAZVVGMwhE71iMqDhyzVlZ0e8f4a9f-1405-45c4-9c49-33ac84907313", getApplicationContext(), BuildConfig.DEBUG))
-                // Get the JS Bundle File via CodePush
-                .setJSBundleFile(CodePush.getJSBundleFile())
-                // ...
-                .build();
-        mReactRootView.startReactApplication(mReactInstanceManager, "MyReactNativeApp", null);
-        setContentView(mReactRootView);
-    }
+    
 
     /**
      * Returns the name of the main component registered from JavaScript.
